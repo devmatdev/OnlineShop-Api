fd7422fa69841b9f382671964ccbb3a6141ca847
devmatdev <devmatdev@gmail.com>
2022-12-04 18:12:40 +0100

    database configuration
    added DB using EFC

---
+++ API/Data/OnlineShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Data
+{
+    using API.Model;
+    using Microsoft.EntityFrameworkCore;
+
+    public class OnlineShopDbContext:DbContext
+    {
+        public OnlineShopDbContext(DbContextOptions<OnlineShopDbContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- API/Model/Product.cs
+++ API/Model/Product.cs
@@ -1,11 +1,25 @@
 ﻿namespace API.Model
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Product
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
-        public string Description { get; set; }
+        
+        public string? Description { get; set; }
+        
         public decimal Price { get; set; }
+        
         public bool IsAvailable { get; set; }
+        
+        public Category Category { get; set; }
+    }
+
+    public enum Category
+    {
+        Kid, Man, Woman
     }
 }
